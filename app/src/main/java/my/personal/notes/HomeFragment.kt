0484744bb6ae8a12fa01d8d74ca5407a8b57a8a3
valchez-dev <valchez.dev@yahoo.com>
2021-12-03 23:00:49 +0300

    Navigation added between Home and Details fragments

---
+++ app/src/main/java/my/personal/notes/HomeFragment.kt
@@ -0,0 +1,49 @@
+package my.personal.notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import my.personal.notes.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController: NavController = Navigation.findNavController(view)
+
+        binding.fabHome.setOnClickListener {
+
+            navController.navigate(R.id.action_homeFragment_to_detailsFragment)
+        }
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
