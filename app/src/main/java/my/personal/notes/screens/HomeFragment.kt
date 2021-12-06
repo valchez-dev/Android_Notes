580d76aa98d8cc64d63643774328b33c9b3e1a72
valchez-dev <valchez.dev@yahoo.com>
2021-12-06 12:22:10 +0300

    HomeFrag navigation to details page setup

--- app/src/main/java/my/personal/notes/screens/HomeFragment.kt
+++ app/src/main/java/my/personal/notes/screens/HomeFragment.kt
@@ -1,28 +1,45 @@
 package my.personal.notes.screens
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.material.transition.MaterialElevationScale
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import my.personal.notes.R
 import my.personal.notes.database.viewmodel.NoteViewModel
 import my.personal.notes.databinding.FragmentHomeBinding
 import my.personal.notes.recyclerview.NoteRvAdapter
 
 
-class HomeFragment : Fragment() {
-
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: NoteViewModel by activityViewModels()
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        exitTransition = MaterialElevationScale(false).apply {
+            duration = 350
+        }
+        enterTransition = MaterialElevationScale(true).apply {
+            duration = 350
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +53,34 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Navigation to details fragment
+        //access to main activity
+        val activity = activity as MainActivity
+
+        //navigation
         val navController: NavController = Navigation.findNavController(view)
 
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(10)
+            activity.window.statusBarColor = Color.WHITE
+            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            activity.window.statusBarColor = Color.parseColor("#9E9D9D")
+        }
 
-
-
+        //add-note button clicked
         binding.fabHome.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_detailsFragment)
+            binding.appBarLayout.visibility = View.INVISIBLE
+            navController.navigate(HomeFragmentDirections.actionHomeFragmentToDetailsFragment())
         }
 
 
+
+
     }
+
+
+
+
+
 
 
     override fun onDestroyView() {
