54bb0eec7eafedf238aff6d13e924aa085aef027
valchez-dev <valchez.dev@yahoo.com>
2021-12-09 22:46:21 +0300

    DiffUtil callback created

---
+++ app/src/main/java/my/personal/notes/adapter/DiffUtilCallback.kt
@@ -0,0 +1,15 @@
+package my.personal.notes.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import my.personal.notes.database.model.Note
+
+class DiffUtilCallback : DiffUtil.ItemCallback<Note>() {
+
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id==newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id==newItem.id
+    }
+}
